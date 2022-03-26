021f3dd2643d4126fd839bc885c349c21dbc0089
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-03-26 15:42:53 +0800

    Update besttrace.sh

--- besttrace.sh
+++ besttrace.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt install wget unzip -y
 wget https://cdn.ipip.net/17mon/besttrace4linux.zip -O /root/besttrace4linux.zip
-unzip /root/besttrace4linux.zip
+unzip -d /root/besttrace4linux /root/besttrace4linux.zip
 cp -f /root/besttrace4linux/besttrace /root/besttrace 
 chmod +x besttrace
 rm -rf /root/besttrace4linux
