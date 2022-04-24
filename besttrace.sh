a732730bef9dad740d301c05fe83f48e2019ae6e
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-04-24 19:10:11 +0800

    Update besttrace.sh

--- besttrace.sh
+++ besttrace.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-apt install wget unzip -y
-wget https://cdn.ipip.net/17mon/besttrace4linux.zip -O /root/besttrace4linux.zip
-unzip -d /root/besttrace4linux /root/besttrace4linux.zip
-cp -f /root/besttrace4linux/besttrace /root/besttrace 
-chmod +x besttrace
-rm -rf /root/besttrace4linux /root/besttrace4linux.zip
+apt install wget -y
+wget https://raw.githubusercontent.com/HXHGTS/GreatDNS/master/bin/besttrace -O /root/besttrace
+chmod +x /root/besttrace
 exit 0
