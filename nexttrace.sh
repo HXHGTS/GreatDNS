a170677aba3cd7dd79b114ad4aba41bd02e00cad
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-06-30 19:41:30 +0800

    Create nexttrace.sh

---
+++ nexttrace.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+apt install wget -y
+wget https://github.com/xgadget-lab/nexttrace/releases/latest/download/nexttrace_linux_amd64 -O /usr/local/bin/nexttrace
+chmod +x /usr/local/bin/nexttrace
+exit 0
