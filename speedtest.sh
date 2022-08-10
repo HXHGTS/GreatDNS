e36ce59fdea254009d8be130950e48d7ca9d5c0d
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-08-10 18:29:52 +0800

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -sSL https://install.speedtest.net/app/cli/install.deb.sh | bash
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | bash
 
 apt install -y speedtest
 
