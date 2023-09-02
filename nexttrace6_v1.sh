469570ad4911d8f372cd4dae12c8284ec7d66fd5
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-09-02 23:31:38 +0800

    Update nexttrace6_v1.sh

--- nexttrace6_v1.sh
+++ nexttrace6_v1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 apt install wget -y
-wget https://summer-poetry-7fa8.qq0mjpmkt9z.workers.dev/xgadget-lab/nexttrace/releases/latest/download/nexttrace_linux_amd64 -O /usr/local/bin/nexttrace
+wget https://summer-poetry-7fa8.qq0mjpmkt9z.workers.dev/nxtrace/Ntrace-V1/releases/latest/download/nexttrace_linux_amd64 -O /usr/local/bin/nexttrace
 chmod +x /usr/local/bin/nexttrace
 exit 0
