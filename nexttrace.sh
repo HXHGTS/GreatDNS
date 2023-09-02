8e817e2554796ce41a0430b2553e5600f3499dd9
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-09-02 23:25:40 +0800

    Update nexttrace.sh

--- nexttrace.sh
+++ nexttrace.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 apt install wget -y
-wget https://github.com/xgadget-lab/nexttrace/releases/latest/download/nexttrace_linux_amd64 -O /usr/local/bin/nexttrace
+wget https://github.com/nxtrace/NTrace-core/releases/latest/download/nexttrace_linux_amd64 -O /usr/local/bin/nexttrace
 chmod +x /usr/local/bin/nexttrace
 exit 0
