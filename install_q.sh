c85ac11a5a5065d179ae5c2d1f4307a6a10441d4
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-10-05 16:48:58 +0800

    Update install_q.sh

--- install_q.sh
+++ install_q.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-echo '正在关闭/卸载sing-box核心. . .'
-
-systemctl stop sing-box
-
-systemctl disable sing-box
-
 echo '正在安装/升级必需插件. . .'
 
 apt update
@@ -32,44 +26,16 @@ export PATH=$PATH:/usr/local/go/bin
 
 go version
 
-echo '正在编译linux版sing-box核心. . .'
+echo '正在编译linux版q. . .'
 
-Singbox_Version=$(curl https://api.github.com/repos/SagerNet/sing-box/releases  | grep "tag_name" | head -n 1 | gawk -F\" '{print $4}')
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go install -ldflags="-s -w -X main.version=release" github.com/natesales/q@latest
 
-env GOOS=linux GOARCH=amd64 GOAMD64=v3 CGO_ENABLED=0 go install -ldflags "-X github.com/sagernet/sing-box/constant.Version=${Singbox_Version}" -v -tags with_quic,with_ech,with_reality_server github.com/sagernet/sing-box/cmd/sing-box@${Singbox_Version}
+echo '正在安装q. . .'
 
-echo '正在安装sing-box核心. . .'
+mv -f /root/go/bin/q /usr/local/bin/
 
-mv -f /root/go/bin/sing-box /usr/local/bin/
+chmod +x /usr/local/bin/q
 
-chmod +x /usr/local/bin/sing-box
-
-Default_ConfDir=/etc/sing-box
-
-Set_ConfDir=/usr/local/etc/sing-box
-
-Default_BinDir=/usr/bin/sing-box
-
-Set_BinDir=/usr/local/bin/sing-box
-
-mkdir -p ${Set_ConfDir}
-
-mkdir -p /var/lib/sing-box
-
-echo '{}' > ${Set_ConfDir}/config.json
-
-curl https://raw.githubusercontent.com/HXHGTS/Singbox-Install/main/service/sing-box-compatible.service > /etc/systemd/system/sing-box.service
-
-curl https://raw.githubusercontent.com/HXHGTS/Singbox-Install/main/service/sing-box-compatible@.service > /etc/systemd/system/sing-box@.service
-
-systemctl daemon-reload
-
-wget -O /var/lib/sing-box/geosite.db https://github.com/soffchen/sing-geosite/releases/latest/download/geosite.db
-
-wget -O /var/lib/sing-box/geoip.db https://github.com/soffchen/sing-geoip/releases/latest/download/geoip.db
-
-systemctl start sing-box
-
-echo 'sing-box核心已安装!'
+echo 'q已安装!'
 
 exit 0
