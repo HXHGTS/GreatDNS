3ee9c973d2714025a7046aad19eb4a6e6b03f524
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-12-09 18:56:56 +0800

    Update install_nali.sh

--- install_nali.sh
+++ install_nali.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-NALI_VERSION=v0.8.0
+apt install curl gawk wget gzip -y
 
-apt install wget gzip -y
+NALI_VERSION=$(curl https://api.github.com/repos/zu1k/nali/tags | grep name | gawk -F\" '{print $4}' | head -n 1)
 
 wget https://github.com/zu1k/nali/releases/download/${NALI_VERSION}/nali-linux-amd64-${NALI_VERSION}.gz -O nali-linux-amd64-${NALI_VERSION}.gz
 
