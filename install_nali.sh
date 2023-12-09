099cf2b0a98c9b0ca4377bbd1167adb88331cc46
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-12-09 18:48:53 +0800

    Update install_nali.sh

--- install_nali.sh
+++ install_nali.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+NALI_VERSION=v0.8.0
+
 apt install wget gzip -y
 
-wget https://github.com/zu1k/nali/releases/download/v0.8.0/nali-linux-amd64-v0.8.0.gz -O nali-linux-amd64-v0.8.0.gz
+wget https://github.com/zu1k/nali/releases/download/${NALI_VERSION}/nali-linux-amd64-${NALI_VERSION}.gz -O nali-linux-amd64-${NALI_VERSION}.gz
 
-gzip -d nali-linux-amd64-v0.8.0.gz
+gzip -d nali-linux-amd64-${NALI_VERSION}.gz
 
-chmod +x nali-linux-amd64-v0.8.0
+chmod +x nali-linux-amd64-${NALI_VERSION}
 
-mv -f nali-linux-amd64-v0.8.0 /usr/local/bin/nali
+mv -f nali-linux-amd64-${NALI_VERSION} /usr/local/bin/nali
 
 echo 'nali已安装!'
 
