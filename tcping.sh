42a2bca1b1786e8f5dd3018a44af05868b222581
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2025-08-10 00:00:52 +0800

    Update tcping.sh

--- tcping.sh
+++ tcping.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
+TCPING_URL=$(curl -L https://api.github.com/repos/nodeseeker/tcping/releases | grep 'browser_download_url'  | grep 'tcping-linux-amd64.zip' |  head -n 1 | awk -F\" {'print $4'})
+
 echo 正在下载tcping. . .
 
 apt update
 
-apt install -y wget
+apt install -y wget unzip
 
-wget -O /opt/tcping-linux-amd64-v0.1.1.tar.gz https://github.com/cloverstd/tcping/releases/download/v0.1.1/tcping-linux-amd64-v0.1.1.tar.gz
+wget -O /var/tmp/tcping-linux-amd64.zip ${TCPING_URL}
 
 echo 正在解压并安装tcping. . .
 
-cd /opt
+unzip -o -d /var/tmp/tcping-linux-amd64 /var/tmp/tcping-linux-amd64.zip
 
-tar -zvxf tcping-linux-amd64-v0.1.1.tar.gz
+rm -f /var/tmp/tcping-linux-amd64.zip
 
-rm -f tcping-linux-amd64-v0.1.1.tar.gz
-
-mv -f tcping /usr/local/bin
+mv -f /var/tmp/tcping-linux-amd64/tcping /usr/local/bin
 
 echo tcping安装完成!
 
