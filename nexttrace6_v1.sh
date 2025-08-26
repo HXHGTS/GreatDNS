dc432fa072dd12b7d1e7aa2e69a8b1231e90d669
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2025-08-26 13:30:39 +0800

    Update nexttrace6_v1.sh

--- nexttrace6_v1.sh
+++ nexttrace6_v1.sh
@@ -2,7 +2,8 @@
 apt install wget -y
 cp -f /etc/resolv.conf /etc/resolv.conf.bak
 echo 'nameserver 2a00:1098:2b::1' > /etc/resolv.conf
-wget https://github.com/nxtrace/Ntrace-V1/releases/latest/download/nexttrace_linux_amd64 -O /usr/local/bin/nexttrace
+NT_VERSION=$(curl https://api.github.com/repos/nxtrace/NTrace-V1/tags | grep name | awk -F\" '{print $4}' | head -n 1 | sed -e 's/v//g')
+wget https://github.com/nxtrace/NTrace-V1/releases/download/v${NT_VERSION}/nexttrace_linux_amd64 -O /usr/local/bin/nexttrace
 mv -f /etc/resolv.conf.bak /etc/resolv.conf
 chmod +x /usr/local/bin/nexttrace
 exit 0
