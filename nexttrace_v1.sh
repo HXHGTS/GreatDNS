9dc7de0dd17d2ccacc89fba3cd4a9ac8ae0f433d
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2025-08-26 13:29:59 +0800

    Update nexttrace_v1.sh

--- nexttrace_v1.sh
+++ nexttrace_v1.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 apt install wget -y
-wget https://github.com/nxtrace/Ntrace-V1/releases/latest/download/nexttrace_linux_amd64 -O /usr/local/bin/nexttrace
+NT_VERSION=$(curl https://api.github.com/repos/nxtrace/NTrace-V1/tags | grep name | awk -F\" '{print $4}' | head -n 1 | sed -e 's/v//g')
+wget https://github.com/nxtrace/NTrace-V1/releases/download/v${NT_VERSION}/nexttrace_linux_amd64 -O /usr/local/bin/nexttrace
 chmod +x /usr/local/bin/nexttrace
 exit 0
