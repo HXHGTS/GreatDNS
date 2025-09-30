211777a902ad0e27104236c99adfe8342ca6368f
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2025-09-30 17:31:32 +0800

    Update install_q.sh

--- install_q.sh
+++ install_q.sh
@@ -2,9 +2,9 @@
 
 apt install -y curl wget tar
 
-Q_VERSION=0.19.2
+# Q_VERSION=0.19.2
 
-# Q_VERSION=$(curl https://api.github.com/repos/natesales/q/tags | grep name | awk -F\" '{print $4}' | head -n 1 | sed -e 's/v//g')
+Q_VERSION=$(curl https://api.github.com/repos/natesales/q/tags | grep name | awk -F\" '{print $4}' | head -n 1 | sed -e 's/v//g')
 
 wget https://github.com/natesales/q/releases/download/v${Q_VERSION}/q_${Q_VERSION}_linux_amd64.tar.gz -O q_${Q_VERSION}_linux_amd64.tar.gz
 
