96cf7bf2a24daf9f608c60ec0508b7a9b8da38ef
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2025-10-09 19:30:12 +0800

    Update nexttrace6.sh

--- nexttrace6.sh
+++ nexttrace6.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 apt install wget -y
-cp -f /etc/resolv.conf /etc/resolv.conf.bak
-echo 'nameserver 2a00:1098:2b::1' > /etc/resolv.conf
-wget https://github.com/nxtrace/NTrace-core/releases/latest/download/nexttrace_linux_amd64 -O /usr/local/bin/nexttrace
-mv -f /etc/resolv.conf.bak /etc/resolv.conf
+#echo 'nameserver 2a00:1098:2b::1' > /etc/resolv.conf
+mv -f /etc/hosts /etc/hosts.bak
+echo '2a00:1098:2b::1:141a:9cd2 api.github.com' > /etc/hosts
+echo '2a00:1098:2b::1:141a:9cd7 github.com' >> /etc/hosts
+NT_URL=$(curl https://api.github.com/repos/nxtrace/NTrace-core/releases | grep browser_download_url | grep nexttrace_linux_amd64 | head -n 1 | awk -F\" '{print $4}')
+wget ${NT_URL} -O /usr/local/bin/nexttrace
+mv -f /etc/hosts.bak /etc/hosts
 chmod +x /usr/local/bin/nexttrace
 exit 0
