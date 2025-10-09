f089bfc4d1513772e085487dddf9b81b50609862
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2025-10-09 19:26:28 +0800

    Update nexttrace6_v1.sh

--- nexttrace6_v1.sh
+++ nexttrace6_v1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt install wget -y
 #echo 'nameserver 2a00:1098:2b::1' > /etc/resolv.conf
-cp -f /etc/hosts /etc/hosts.bak
+mv -f /etc/hosts /etc/hosts.bak
 echo '2a00:1098:2b::1:141a:9cd2 api.github.com' > /etc/hosts
 echo '2a00:1098:2b::1:141a:9cd7 github.com' >> /etc/hosts
 NT_URL=$(curl https://api.github.com/repos/nxtrace/NTrace-V1/releases | grep browser_download_url | grep nexttrace_linux_amd64 | head -n 1 | awk -F\" '{print $4}')
