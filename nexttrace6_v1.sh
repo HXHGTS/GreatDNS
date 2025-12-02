15d978627d8cb67a82645cd4aaf49b90e9e51557
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2025-12-02 15:32:38 +0800

    Update nexttrace6_v1.sh

--- nexttrace6_v1.sh
+++ nexttrace6_v1.sh
@@ -3,9 +3,11 @@ apt install wget -y
 #echo 'nameserver 2a00:1098:2b::1' > /etc/resolv.conf
 mv -f /etc/hosts /etc/hosts.bak
 echo '2a00:1098:2b::1:141a:9cd2 api.github.com' > /etc/hosts
-echo '2a00:1098:2b::1:141a:9cd7 github.com' >> /etc/hosts
-echo '2a00:1098:2b::1:b9c7:6c85 release-assets.githubusercontent.com' >> /etc/hosts
-NT_URL=$(curl https://api.github.com/repos/nxtrace/NTrace-V1/releases | grep browser_download_url | grep nexttrace_linux_amd64 | head -n 1 | awk -F\" '{print $4}')
+# echo '2a00:1098:2b::1:141a:9cd7 github.com' >> /etc/hosts
+# echo '2a00:1098:2b::1:b9c7:6c85 release-assets.githubusercontent.com' >> /etc/hosts
+# NT_URL=$(curl https://api.github.com/repos/nxtrace/NTrace-core/releases | grep browser_download_url | grep nexttrace_linux_amd64 | head -n 1 | awk -F\" '{print $4}')
+NT_VERSION=$(curl https://api.github.com/repos/nxtrace/NTrace-core/releases | grep tag_name | head -n 1 | awk -F\" '{print $4}')
+NT_URL=$(echo "https://dl-r2.nxtrace.org/dist/v1/${NT_VERSION}/nexttrace_linux_amd64")
 wget ${NT_URL} -O /usr/local/bin/nexttrace
 mv -f /etc/hosts.bak /etc/hosts
 chmod +x /usr/local/bin/nexttrace
