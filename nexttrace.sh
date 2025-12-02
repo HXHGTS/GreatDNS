a2486396709148a1fc5055ead14e376b55a88876
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2025-12-02 15:30:35 +0800

    Update nexttrace.sh

--- nexttrace.sh
+++ nexttrace.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 apt install wget -y
-NT_URL=$(curl https://api.github.com/repos/nxtrace/NTrace-core/releases | grep browser_download_url | grep nexttrace_linux_amd64 | head -n 1 | awk -F\" '{print $4}')
+NT_VERSION=$(curl https://api.github.com/repos/nxtrace/NTrace-core/releases | grep tag_name | head -n 1 | awk -F\" '{print $4}')
+NT_URL=$(echo "https://dl-r2.nxtrace.org/dist/core/${NT_VERSION}/nexttrace_linux_amd64")
 wget ${NT_URL} -O /usr/local/bin/nexttrace
 chmod +x /usr/local/bin/nexttrace
 exit 0
