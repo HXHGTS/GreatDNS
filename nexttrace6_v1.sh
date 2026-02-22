805eca91f252a480e5299d71756fdb250bb33d21
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2026-02-22 19:31:32 +0800

    Update nexttrace6_v1.sh

--- nexttrace6_v1.sh
+++ nexttrace6_v1.sh
@@ -8,7 +8,7 @@ echo '2a00:1098:2b::1:141a:9cd2 api.github.com' > /etc/hosts
 # NT_URL=$(curl https://api.github.com/repos/nxtrace/NTrace-core/releases | grep browser_download_url | grep nexttrace_linux_amd64 | head -n 1 | awk -F\" '{print $4}')
 NT_VERSION=$(curl https://api.github.com/repos/nxtrace/NTrace-core/releases | grep tag_name | head -n 1 | awk -F\" '{print $4}')
 NT_URL=$(echo "https://dl-r2.nxtrace.org/dist/v1/${NT_VERSION}/nexttrace_linux_amd64")
-wget ${NT_URL} -O /usr/local/bin/nexttrace
+curl -o /usr/local/bin/nexttrace -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36' ${NT_URL} 
 mv -f /etc/hosts.bak /etc/hosts
 chmod +x /usr/local/bin/nexttrace
 exit 0
