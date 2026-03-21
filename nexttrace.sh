fd652dd46e6b5072eb88cb8926755bacca7ccbac
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2026-03-21 12:14:39 +0800

    Add files via upload

--- nexttrace.sh
+++ nexttrace.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 NT_VERSION=$(curl https://api.github.com/repos/nxtrace/NTrace-core/releases | grep tag_name | head -n 1 | awk -F\" '{print $4}')
-NT_URL=$(echo "https://dl-r2.nxtrace.org/dist/core/${NT_VERSION}/nexttrace_linux_amd64")
-curl -o /usr/local/bin/nexttrace -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36' ${NT_URL} 
+NT_URL=$(echo "https://github.com/nxtrace/NTrace-core/releases/download/${NT_VERSION}/nexttrace_linux_amd64")
+curl -o /usr/local/bin/nexttrace -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36' -sL ${NT_URL} 
 chmod +x /usr/local/bin/nexttrace
 exit 0
