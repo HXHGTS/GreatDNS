3a85a778f14d92982525230ab5503801c1698882
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2026-04-23 22:26:50 +0800

    Update install_q.sh

--- install_q.sh
+++ install_q.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-apt install -y curl wget tar
+apt install -y curl tar
 
 # Q_VERSION=0.19.2
 
 Q_VERSION=$(curl https://api.github.com/repos/natesales/q/tags | grep name | awk -F\" '{print $4}' | head -n 1 | sed -e 's/v//g')
 
-wget https://github.com/natesales/q/releases/download/v${Q_VERSION}/q_${Q_VERSION}_linux_amd64.tar.gz -O q_${Q_VERSION}_linux_amd64.tar.gz
+curl -o q_${Q_VERSION}_linux_amd64.tar.gz -sSL https://github.com/natesales/q/releases/download/v${Q_VERSION}/q_${Q_VERSION}_linux_amd64.tar.gz
 
 mkdir -p q_${Q_VERSION}_linux_amd64 && tar -xzvf q_${Q_VERSION}_linux_amd64.tar.gz -C q_${Q_VERSION}_linux_amd64/
 
