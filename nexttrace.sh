d185da86271c5a3edf050b9771a16cb3c1c58b2a
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2026-04-23 21:36:36 +0800

    Update nexttrace.sh

--- nexttrace.sh
+++ nexttrace.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-NT_VERSION=$(curl https://api.github.com/repos/nxtrace/NTrace-core/releases | grep tag_name | head -n 1 | awk -F\" '{print $4}')
-NT_URL=$(echo "https://github.com/nxtrace/NTrace-core/releases/download/${NT_VERSION}/nexttrace_linux_amd64")
-curl -o /usr/local/bin/nexttrace -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36' -sL ${NT_URL} 
-chmod +x /usr/local/bin/nexttrace
+install -d -m 0755 /etc/apt/keyrings
+curl -fsSL -o /tmp/nexttrace-archive-keyring.gpg https://github.com/nxtrace/nexttrace-debs/releases/latest/download/nexttrace-archive-keyring.gpg
+install -m 0644 /tmp/nexttrace-archive-keyring.gpg /etc/apt/keyrings/nexttrace.gpg
+rm -f /tmp/nexttrace-archive-keyring.gpg
+printf '%s\n' 'Types: deb' 'URIs: https://github.com/nxtrace/nexttrace-debs/releases/latest/download/' 'Suites: ./' 'Signed-By: /etc/apt/keyrings/nexttrace.gpg' | tee /etc/apt/sources.list.d/nexttrace.sources >/dev/null
+apt update
+apt install nexttrace
 exit 0
